a7aaf62f120f71dccd7e83ce8f54564244cbf53f
Guilherme Tadeu <gtadeu0147@gmail.com>
2021-04-23 12:21:01 -0300

    Setup do projeto

---
+++ src/test/java/com/orange/proposal/ProposalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.proposal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProposalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
